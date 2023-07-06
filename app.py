2f5a4b4b14998a9efbf1a1f405c7c098d48e125c
ChristTheKing7 <ssembatyadavid54@gmail.com>
2023-07-06 11:05:42 +0300

    Another commit

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, SubmitField,SelectField,DateField
 from wtforms.validators import DataRequired, email_validator, Length, EqualTo
 from datetime import datetime
+import feedparser
+
 
 from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = "CTKDANTE"
@@ -15,6 +19,8 @@ app.config['MYSQL_PASSWORD'] = "dante"
 app.config['MYSQL_DB'] = "users_db"
 
 db = MySQL(app)
+
+
 
 # creating the form class
 class signupForm(FlaskForm):
@@ -31,10 +37,11 @@ class signupForm(FlaskForm):
 class user_input(FlaskForm):
     commodity = SelectField('Choose commodity To foreast', choices=[('sugar', 'Sugar'),
                  ('maize', 'Maize'), ('rice', 'Rice'), ('Beans', 'Beans')])
-    
+    #forecasted_date = DateField('Enter Date to forecast', format="%Y-%m-%d")
     forecasted_date = SelectField('Choose Month in which to forecast', choices=[('2023-02-15', 'January'),
     ('2023-02-15', 'Febuary'), ('2023-03-15', 'March'), ('2023-04-15', 'April'), ('2023-05-15', 'May'), ('2023-06-15', 'June'), 
     ('2023-07-15', 'July'), ('2023-08-15', 'August'), ('2023-09-15', 'September'), ('2023-10-15', 'October'), ('2023-11-15', 'November'), ('2023-12-15', 'December')])
+    
     submit = SubmitField('Forecast')
     
 
@@ -95,6 +102,7 @@ def forecast():
     if form.validate_on_submit():
         choice = form.commodity.data
         date_choice = form.forecasted_date.data
+        
         flash('Here is Your forecast of ' +choice+ ' For date ' +date_choice)
         print(choice +" is for month "+ date_choice )
     
@@ -121,9 +129,34 @@ def about():
 def summary():
     return render_template("summary.html")
 
+#creating dynamic tables
+headings = ('    ','Owino', 'Mbale', 'Masaka', 'Gulu')
+data = (
+    ('Sugar', '', '', '', ''),
+    ('Rice', '', '', '', ''),
+    ('Maize flour', '', '', '', ''),
+    ('Beans', '', '', '', '')
+
+)
+commodity_heading = ('Sugar', 'Rice', 'Maize Flour', 'Beans')
+lowest_prx = 2000
+
 @app.route('/Key_Stats')
 def key_stats():
-    return render_template("stats.html")
+#creating the feed parser
+    feed_url = 'http://feeds.bbci.co.uk/news/business/rss.xml'
+    feed = feedparser.parse(feed_url)
+
+# Access the feed data
+    feed_title = feed.feed.title
+    entries = feed.entries
+    return render_template("stats.html", headings = headings, data = data,
+    
+    commodity_heading = commodity_heading,
+    lowest_prx = lowest_prx,
+    feed_title = feed_title,
+    entries = entries
+    )
 
 @app.route('/Explanations')
 def explanations():
